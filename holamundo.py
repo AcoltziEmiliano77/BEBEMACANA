312b905635743d18d259f6fe7c035942164ac51f
Acoltzi Mejia Emiliano <emilianoacoltzi11@gmail.com>
2025-06-15 14:23:32 -0600

    Update holamundo.py

--- holamundo.py
+++ holamundo.py
@@ -1,5 +1,5 @@
 import tkinter as tk
 from tkinter import font
 ventanaprincipal = tk.Tk()
-tk.Label(ventanaprincipal, text="Hola mundo").pack()  
+tk.Label(ventanaprincipal, text="Hola mundo mundial,hola que hace perraquin?").pack()  
 ventanaprincipal.mainloop()
